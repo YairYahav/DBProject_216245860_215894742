155aed6618e532cc2d9db4782022c662a09bfbdf
YairYahav <154914526+YairYahav@users.noreply.github.com>
2025-05-05 23:54:50 +0300

    Update selectall.sql

--- code/sql/selectall.sql
+++ code/sql/selectall.sql
@@ -3,20 +3,23 @@
 -- בחירת כל הנתונים מטבלת הלקוחות
 SELECT * FROM Customer;
 
--- בחירת כל הנתונים מטבלת הכתובות
-SELECT * FROM Address;
+-- בחירת כל הנתונים מטבלת המכשירים
+SELECT * FROM Devices;
 
--- בחירת כל הנתונים מטבלת אנשי הקשר
-SELECT * FROM Contact;
+-- בחירת כל הנתונים מטבלת היסטוריית הצפייה
+SELECT * FROM WatchHistory;
 
--- בחירת כל הנתונים מטבלת המסמכים של הלקוחות
-SELECT * FROM CustomerDocument;
+-- בחירת כל הנתונים מטבלת המועדפים
+SELECT * FROM Favorites;
 
--- בחירת כל הנתונים מטבלת ההערות של הלקוחות
-SELECT * FROM CustomerNote;
+-- בחירת כל הנתונים מטבלת התשלום
+SELECT * FROM Payment;
 
--- בחירת כל הנתונים מטבלת סגמנטי הלקוחות
-SELECT * FROM CustomerSegment;
+-- בחירת כל הנתונים מטבלת הפרופיל
+SELECT * FROM Profile;
 
--- בחירת כל הנתונים מטבלת שיוך לקוחות לסגמנטים
-SELECT * FROM CustomerSegmentAssignment;
+-- בחירת כל הנתונים מטבלת התגובות
+SELECT * FROM Reviews;
+
+-- בחירת כל הנתונים מטבלת הסומן כמועדפים
+SELECT * FROM MarksAsFavorite;
